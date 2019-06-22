9f5dc7ec47e1cdd2b48ca8bdae484c2b47ef2cd8
Rodrigo Matos <rodrigo.matos9@hotmail.com>
2019-06-22 18:51:17 -0300

    Add files via upload

--- utebo.py
+++ utebo.py
@@ -11,6 +11,12 @@ placarJ2 = 0
 def main():
     global placarJ1
     global placarJ2
+    digitos = 1
+    auxPlacarJ1 = placarJ1
+    while (auxPlacarJ1 >= 10):
+        auxPlacarJ1 = auxPlacarJ1 / 10
+        digitos += 1
+    print(digitos)
     pygame.init()
     tempo = 0
     tempo1 = 0
@@ -23,9 +29,13 @@ def main():
     infoTela = pygame.display.Info()
     largura = infoTela.current_w
     altura = infoTela.current_h
-    screen = pygame.display.set_mode((800, 600))
+    #screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((largura, altura), pygame.FULLSCREEN)
     fontePlacar = pygame.font.SysFont("monospace", 90, 1)
-    textoPlacar = str(placarJ1) + " " + str(placarJ2)
+    espacos = ""
+    for i in range(digitos):
+        espacos += " "
+    textoPlacar = str(placarJ1) + espacos + str(placarJ2)
     placar = fontePlacar.render(textoPlacar, 0, (0, 0, 0))
     pygame.display.set_caption("UTEBO - %d X %d" % (placarJ1, placarJ2))
     done = True
@@ -35,12 +45,17 @@ def main():
     tj = 20 # Tamanho do Jogador
     x = [largura - largura//15 - tj, largura//15]
     y = [altura / 2 - tj//2 , altura / 2 - tj//2]
-    vel = 4
+    vel1 = 20
+    vel2 = 20
+    multEnergia = 1.5
     r = 14
     tb = r * 2
-
+    energiaJ1 = 100
+    energiaJ2 = 100
+    custoEnergia = 1
     clock = pygame.time.Clock()
-
+    corrida1 = False
+    corrida2 = False
     while done:
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
@@ -51,6 +66,26 @@ def main():
                 chute = True
             if (event.type == pygame.KEYUP and event.key == pygame.K_p):
                 chuteAcabou = True
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_l and energiaJ1 >= custoEnergia):
+                vel1 = vel1 * multEnergia
+                corrida1 = True
+            if (event.type == pygame.KEYUP and event.key == pygame.K_l and corrida1):
+                vel1 = vel1 / multEnergia
+                corrida1 = False
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_g and energiaJ2 >= custoEnergia):
+                vel2 = vel2 * multEnergia
+                corrida2 = True
+            #if(hasattr(event, "key")):
+            #    print(event.type == pygame.KEYUP, event.key == pygame.K_g, corrida2, energiaJ2 < custoEnergia, corrida2)
+            if (event.type == pygame.KEYUP and event.key == pygame.K_g and corrida2):
+                vel2 = vel2 / multEnergia
+                corrida2 = False
+        if(energiaJ1 < custoEnergia and corrida1):
+            vel1 = vel1 / multEnergia
+            corrida1 = False
+        if(energiaJ2 < custoEnergia and corrida2):
+            vel2 = vel2 / multEnergia
+            corrida2 = False
         # Bola enconsta no lado esquerdo
         if(t - r < 0):
             if(not(t - r < 0 and k > altura//4 and k < 3 * altura//4)):
@@ -85,38 +120,44 @@ def main():
         else:
             distTY1 = (k - r) - (y[1] + tj)
         pressed = pygame.key.get_pressed()
+        # Energia / Stamina
+        if (pressed[pygame.K_l] and energiaJ1 >= custoEnergia):
+            energiaJ1 -= custoEnergia
+        if(pressed[pygame.K_g] and energiaJ2 >= custoEnergia):
+            energiaJ2 -= custoEnergia
         # Movimento dos Jogadores (J1 e J2)
         if pressed[pygame.K_ESCAPE]:
             done = False
         if pressed[pygame.K_UP]:
-            if(not(distX <= tj and abs(y[0] - vel - y[1]) <= tj and y[0] > y[1]) and (not(y[0] <= 0))):
-                y[0] -= vel
+            if(not(distX <= tj and abs(y[0] - vel1 - y[1]) <= tj and y[0] > y[1]) and (not(y[0] <= 0))):
+                y[0] -= vel1
         if pressed[pygame.K_DOWN]:
-            if(not(distX <= tj and abs(y[0] + vel - y[1]) <= tj and y[0] < y[1]) and (not(y[0]+tj >= altura))):
-                y[0] += vel
+            if(not(distX <= tj and abs(y[0] + vel1 - y[1]) <= tj and y[0] < y[1]) and (not(y[0]+tj >= altura))):
+                y[0] += vel1
         if pressed[pygame.K_LEFT]:
-            if(not(abs(x[0] - vel - x[1]) <= tj and distY <= tj and x[0] > x[1]) and (not(x[0] <= 0))):
-                x[0] -= vel
+            if(not(abs(x[0] - vel1 - x[1]) <= tj and distY <= tj and x[0] > x[1]) and (not(x[0] <= 0))):
+                x[0] -= vel1
         if pressed[pygame.K_RIGHT]:
-            if(not(abs(x[0] + vel - x[1]) <= tj and distY <= tj and x[0] < x[1]) and (not(x[0] + tj >= largura))):
-                x[0] += vel
+            if(not(abs(x[0] + vel1 - x[1]) <= tj and distY <= tj and x[0] < x[1]) and (not(x[0] + tj >= largura))):
+                x[0] += vel1
         if pressed[pygame.K_w]:
-            if(not(distX <= tj and abs(y[0] - (y[1] - vel)) <= tj and y[1] > y[0]) and (not(y[1] <= 0))):
-                y[1] -= vel
+            if(not(distX <= tj and abs(y[0] - (y[1] - vel2)) <= tj and y[1] > y[0]) and (not(y[1] <= 0))):
+                y[1] -= vel2
         if pressed[pygame.K_s]:
-            if(not(distX <= tj and abs(y[0] - (y[1] + vel)) <= tj and y[1] < y[0]) and (not(y[1]+tj >= altura))):
-                y[1] += vel
+            if(not(distX <= tj and abs(y[0] - (y[1] + vel2)) <= tj and y[1] < y[0]) and (not(y[1]+tj >= altura))):
+                y[1] += vel2
         if pressed[pygame.K_a]:
-            if(not(abs(x[0] - (x[1] - vel)) <= tj and distY <= tj and x[1] > x[0]) and (not(x[1] <= 0))):
-                x[1] -= vel
+            if(not(abs(x[0] - (x[1] - vel2)) <= tj and distY <= tj and x[1] > x[0]) and (not(x[1] <= 0))):
+                x[1] -= vel2
         if pressed[pygame.K_d]:
-            if(not(abs(x[0] - (x[1] + vel)) <= tj and distY <= tj and x[1] < x[0]) and (not(x[1]+tj >= largura))):
-                x[1] += vel
+            if(not(abs(x[0] - (x[1] + vel2)) <= tj and distY <= tj and x[1] < x[0]) and (not(x[1]+tj >= largura))):
+                x[1] += vel2
         # Movimento da Bola
-        velBola = ceil(vel)
+        velBola1 = ceil(vel1)
+        velBola2 = ceil(vel2)
         # Movimento da Bola pra Direita (J1)
         if(distTX<=0 and x[0]<t and distTY <= -4):
-            t += velBola
+            t += velBola1
             if pressed[pygame.K_RCTRL]:
 ##                clock = pygame.time.Clock()
 ##                clock.tick()
@@ -138,8 +179,8 @@ def main():
                     print(tempo2 - tempo1)
                 p = True
         # Movimento da Bola pra Esquerda (J1)
-        if(distTX<=0 and x[0]>t and distTY <= -(velBola)):
-            t -= velBola
+        if(distTX<=0 and x[0]>t and distTY <= -(velBola1)):
+            t -= velBola1
 ##            if pressed[pygame.K_RCTRL]:
 ####                clock = pygame.time.Clock()
 ####                clock.tick()
@@ -161,32 +202,32 @@ def main():
                     velchute = 8
                 else:    
                     tempoFinal = time.time()
-                    tempoPercorrido = int(tempoFinal - tempoInicial)
+                    tempoPercorrido = int(round((tempoFinal - tempoInicial), 3) * 1000)
                     print(tempoPercorrido)
                     chuteAcabou = False
-                    velchute = 12 + 8*tempoPercorrido
+                    velchute = tempoPercorrido // 30
                 p = True
         elif(distTX>= 50):
             tempoInicial = 0
             tempoFinal = 1
         # Movimento da Bola pra Direita (J2)
-        if(distTX1<=0 and x[1]<t and distTY1 <= -(velBola)):
-            t += velBola
+        if(distTX1<=0 and x[1]<t and distTY1 <= -(velBola2)):
+            t += velBola2
         # Movimento da Bola pra Esquerda (J2)
-        if(distTX1<=0 and x[1]>t and distTY1 <= -(velBola)):
-            t -= velBola
+        if(distTX1<=0 and x[1]>t and distTY1 <= -(velBola2)):
+            t -= velBola2
         # Movimento da Bola pra Baixo (J1)
-        if(distTY<=0 and y[0]<k and distTX <= -(velBola)):
-            k += velBola
+        if(distTY<=0 and y[0]<k and distTX <= -(velBola1)):
+            k += velBola1
         # Movimento da Bola pra Cima (J1)
-        if(distTY<=0 and y[0]>k and distTX <= -(velBola)):
-            k -= velBola
+        if(distTY<=0 and y[0]>k and distTX <= -(velBola1)):
+            k -= velBola1
         # Movimento da Bola pra Baixo (J2)
-        if(distTY1 <= 0 and y[1]<k and distTX1 <= -(velBola)):
-            k += velBola
+        if(distTY1 <= 0 and y[1]<k and distTX1 <= -(velBola2)):
+            k += velBola2
         # Movimento da Bola pra Cima (J2)
-        if(distTY1 <= 0 and y[1]>k and distTX1 <= -(velBola)):
-            k -= velBola
+        if(distTY1 <= 0 and y[1]>k and distTX1 <= -(velBola2)):
+            k -= velBola2
         # Gol do J2
         if(t + r > largura and k > altura//4 and k < 3 * altura // 4):
             print("GOL DO CAIO") # colocar isso no display, ou apagar
@@ -221,6 +262,7 @@ def main():
             white = (255,255,255)
             darkGreen = (0,100,0)
             cinza = (100, 100, 100)
+            yellow = (255, 255, 0)
         # Gramado
         pygame.draw.rect(screen,darkGreen,pygame.Rect(0,0,largura,altura))
         # Círcunferência Central
@@ -241,10 +283,22 @@ def main():
         # Bola
         pygame.draw.circle(screen, cinza, [t,k], r)
         pygame.draw.circle(screen, cinza, [t,k], r)
+        # Barra de Energia Direita
+        pygame.draw.rect(screen, yellow, pygame.Rect(largura - 50 - energiaJ1 * 2, 50, energiaJ1 * 2, 50))
+        # Barra de Energia Esquerda
+        pygame.draw.rect(screen, yellow, pygame.Rect(50, 50, energiaJ2 * 2, 50))
         # Placar
-        screen.blit(placar, (largura//2 - 80, 60))
-        
+        screen.blit(placar, (largura//2 - 80 * digitos, 60))
         pygame.display.flip()
+        # Energia / Stamina
+        if(energiaJ1 < 100):
+            energiaJ1 += custoEnergia / 7
+        else:
+            energiaJ1 = 100
+        if(energiaJ2 < 100):
+            energiaJ2 += custoEnergia / 7
+        else:
+            energiaJ2 = 100
         clock.tick(60)
 
 main()
